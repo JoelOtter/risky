9327986645efdc6d58251f9532c44f13808841ed
Joel Auterson <joel.auterson@googlemail.com>
2015-02-07 21:37:34 +0000

    Cleanup, add auto mode

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+var frame, range, interval;
+
 function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
 
+function setBlur(blurPc) {
+    frame.style['-webkit-filter'] = 'blur(' + (blurPc / 100 * 50) + 'px)';
+}
+
 function changeBlur(e) {
     var blurPc = 100 - e.target.value;
-    document.getElementById('view').style['-webkit-filter'] = 'blur(' + (blurPc / 100 * 50) + 'px)';
+    setBlur(blurPc);
+}
+
+function decreaseBlur() {
+    var blur = 100 - range.value;
+    blur -= 10;
+    if (blur < 0) blur = 0;
+    range.value = 100 - blur;
+    setBlur(blur);
+}
+
+function toggleAuto(e) {
+    var checked = e.target.checked;
+    if (checked) {
+        interval = setInterval(decreaseBlur, 1000);
+    } else {
+        clearInterval(interval);
+    }
+    range.disabled = checked;
 }
 
 window.onload = function() {
     var url = getURLParameter('url');
-    document.getElementById('view').src = url;
-    document.getElementById('view').style['-webkit-filter'] = 'blur(50px)';
-    document.getElementById('blurRange').addEventListener('input', changeBlur);
+    frame = document.getElementById('view');
+    range = document.getElementById('blurRange');
+    frame.src = url;
+    frame.style['-webkit-filter'] = 'blur(50px)';
+    range.addEventListener('input', changeBlur);
+    document.getElementById('autoCheck').addEventListener('change', toggleAuto);
 };
