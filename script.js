d25e298bb0f6278cb2ceb6b5432307439f847e8b
Joel Auterson <joel.auterson@googlemail.com>
2015-02-07 17:44:58 +0000

    Add slider, unblurring

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
 
+function changeBlur(e) {
+    var blurPc = 100 - e.target.value;
+    document.getElementById('view').style['-webkit-filter'] = 'blur(' + (blurPc / 100 * 50) + 'px)';
+}
+
 window.onload = function() {
     var url = getURLParameter('url');
     document.getElementById('view').src = url;
-    document.getElementById('view').style['-webkit-filter'] = 'blur(2px)';
+    document.getElementById('view').style['-webkit-filter'] = 'blur(50px)';
+    document.getElementById('blurRange').addEventListener('input', changeBlur);
 };
